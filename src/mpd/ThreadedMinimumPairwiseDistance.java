f104f8fd9e21632147b3398d8fee27cc8d95f083
plumstyle <anxxx154@morris.umn.edu>
2018-11-08 11:01:48 -0600

    Create inner classes for threads
    Classes for each type of triangle have
    been added with different loop bounds.
    In addition, threads for each of these
    classes are instantiated, are started,
    and wait for each other to finish. A
    global result variable has also been
    added.

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -4,7 +4,128 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance 
 
     @Override
     public long minimumPairwiseDistance(int[] values) {
-        throw new UnsupportedOperationException();
+        long globalResult = Integer.MAX_VALUE;
+
+        Thread LowerLeftThread = new Thread(new LowerLeft(values, globalResult));
+        Thread LowerRightThread = new Thread(new LowerRight(values, globalResult));
+        Thread MiddleThread = new Thread(new Middle(values, globalResult));
+        Thread TopRightThread = new Thread(new TopRight(values, globalResult));
+
+        LowerLeftThread.start();
+        LowerRightThread.start();
+        MiddleThread.start();
+        TopRightThread.start();
+
+        try {
+            LowerLeftThread.join();
+            LowerRightThread.join();
+            MiddleThread.join();
+            TopRightThread.join();
+        } catch (Exception e){
+            System.out.println("There was an interrupted exception");
+        }
+        return globalResult;
+
     }
 
+
+    public class LowerLeft implements Runnable {
+        long result = Integer.MAX_VALUE;
+        int[] value;
+        long globalResult;
+
+        public LowerLeft(int[] values, long globalResult) {
+            this.value = values;
+            this.globalResult = globalResult;
+        }
+        public void run() {
+            for (int i = 0; i < (value.length / 2); ++i) {
+                for (int j = 0; j < i; ++j) {
+                    // Gives us all the pairs (i, j) where 0 <= j < i < values.length
+                    long diff = Math.abs(value[i] - value[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+            if (result < globalResult) {
+                globalResult = result;
+            }
+        }
+    }
+
+    public class LowerRight implements Runnable {
+        long result = Integer.MAX_VALUE;
+        int[] value;
+        long globalResult;
+
+        public LowerRight(int[] values, long globalResult) {
+            this.value = values;
+            this.globalResult = globalResult;
+        }
+        public void run() {
+            for (int i = (value.length / 2); i < value.length; ++i) {
+                for (int j = 0; j + (value.length / 2) < i; ++j) {
+                    // Gives us all the pairs (i, j) where 0 <= j < i < values.length
+                    long diff = Math.abs(value[i] - value[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+            if (result < globalResult) {
+                globalResult = result;
+            }
+        }
+    }
+
+    public class Middle implements Runnable {
+        long result = Integer.MAX_VALUE;
+        int[] value;
+        long globalResult;
+
+        public Middle(int[] values, long globalResult) {
+            this.value = value;
+            this.globalResult = globalResult;
+        }
+        public void run() {
+            for (int i = (value.length / 2); i < value.length; ++i) {
+                for (int j = (value.length / 2); (value.length / 2) - j < i; ++j) {
+                    // Gives us all the pairs (i, j) where 0 <= j < i < values.length
+                    long diff = Math.abs(value[i] - value[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+            if (result < globalResult) {
+                globalResult = result;
+            }
+        }
+    }
+
+    public class TopRight implements Runnable {
+        long result = Integer.MAX_VALUE;
+        int[] value;
+        long globalResult;
+
+        public TopRight(int[] values, long globalResult) {
+            this.value = values;
+            this.globalResult = globalResult;
+        }
+        public void run() {
+            for (int i = (value.length / 2); i < value.length; ++i) {
+                for (int j = (value.length / 2); j < i; ++j) {
+                    // Gives us all the pairs (i, j) where 0 <= j < i < values.length
+                    long diff = Math.abs(value[i] - value[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+            if (result < globalResult) {
+                globalResult = result;
+            }
+        }
+    }
 }
